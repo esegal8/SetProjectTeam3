88750b51abe166032bb0d48fdd6bdc3e88ac0184
rxu8 <rxu8@students.d125.org>
2016-03-10 10:19:34 -0600

    all methods headers are written, toString needs to be reviewed

--- Group1/Deck.java
+++ Group1/Deck.java
@@ -24,13 +24,37 @@ public class Deck
     {
         deck= new ArrayList<Card>();
     }
+    public Deck(int numCard)
+    {
+        
+    }
+    public int getNumCards()
+    {
+        return 1;
+    }
+    public boolean isEmpty()
+    {
+    }
+    public void add (Card card)
+    {
+        
+    }
+    public Card takeTop()
+    {
+    }
+    public void shuffle()
+    {
+    }
+    public void sort()
+    {
+    }
     public String toString()
     {
         String ret="";
         Iterator<Card> iter= deck.iterator();
         while (iter.hasNext())
         {
-            ret += System.out.print(iter.next().toString()+ " ");
+            ret += System.out.print(iter.next().toString()+ "\n");
         }
         return ret;
     }
