95f7877144d04cfc8d4c610d36df43dc7e93c472
esegal8 <esegal8@students.d125.org>
2016-03-10 10:45:20 -0600

    Group 1 task 2 classes complete

--- Group1/ZetDeck.java
+++ Group1/ZetDeck.java
@@ -5,6 +5,18 @@ public class ZetDeck extends Deck
     
     public ZetDeck()
     {
-        
+        for (int x=0;x<3;x++)
+        {
+            for(int y=0;y<3;y++)
+            {
+                for(int z=0;z<3;z++)
+                {
+                    for(int p=0;p<3;p++)
+                    {
+                        deck.add(ZetCard(x,y,z,p));
+                    }
+                }
+            }
+        }
     }
 }
