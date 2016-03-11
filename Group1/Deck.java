20824b9d3b5cebcedf5cad09710e38677a75f5e3
jmontei8 <jmontei8@students.d125.org>
2016-03-11 10:40:29 -0600

    Check

--- Group1/Deck.java
+++ Group1/Deck.java
@@ -4,16 +4,16 @@
 
 /*
 
-Tips:
+  Tips:
 
-- Use an ArrayList<Card> to hold the cards.
-- Add and remove cards at the end of the list.
-- Use Collections.shuffle and Collections.sort to shuffle and sort the deck,
-or write your own methods.  If you write your own, use selection sort
-to sort and a similar algorithm to shuffle.  Use Math.random().
-- In the toString method, separate strings for individual cards with "\n".
+  - Use an ArrayList<Card> to hold the cards.
+  - Add and remove cards at the end of the list.
+  - Use Collections.shuffle and Collections.sort to shuffle and sort the deck,
+    or write your own methods.  If you write your own, use selection sort
+    to sort and a similar algorithm to shuffle.  Use Math.random().
+  - In the toString method, separate strings for individual cards with "\n".
 
- */
+*/
 
 import java.util.*;
 
@@ -24,59 +24,41 @@ public class Deck
     {
         ArrayList<Card> deck= new ArrayList<Card>();
     }
-
     public Deck(int numCard)
     {
         ArrayList<Card> deck= new ArrayList<Card>(numCard);
     }
-
     public int getNumCards()
     {
-        return deck.size();
+        return 1;
     }
-
     public boolean isEmpty()
     {
-        if (deck.get(0)!= null)
-        {
-            return true;
-        }
-        return false;
     }
-
     public void add (Card c)
     {
         deck.add(0,c);
-        shuffle();
     }
-
     public Card takeTop()
     {
-        Card random= deck.get(0);
-        return random;
-
     }
-
     public void shuffle()
     {
-        Collections.shuffle(deck);
-
     }
-
     public void sort()
     {
-        Collections.sort(deck);
     }
-
     public String toString()
     {
         String ret="";
         Iterator<Card> iter= deck.iterator();
         while (iter.hasNext())
         {
-            ret= ret+ iter.next().toString(); 
+            ret += System.out.print(iter.next()+ "\n");
         }
         return ret;
-    }
 
+    }
+   
+  
 }
--- Group1/ZetCard.java
+++ Group1/ZetCard.java
@@ -1,7 +1,7 @@
 //Elliot and Varun
 public class ZetCard extends Card
 {
-
+    private int ID;
     private int num;
     private int shape;
     private int fill;
@@ -11,33 +11,33 @@ public class ZetCard extends Card
     private final String[] colorA = {null,"red","green","blue"};
     public ZetCard(int n, int s, int f, int c)
     {
-
+        super(ID);//call parent constructor
         num = n;
         shape = s;
         fill = f;
         color = c;
     }
-
+    
     public int getNumber()
     {
         return num;
     }
-
+    
     public int getShape()
     {
         return shape;
     }
-
+    
     public int getFill()
     {
         return fill;
     }
-
+    
     public int getColor()
     {
         return color;
     }
-
+    
     public String toString()
     {
         return ("This card has "+num+" "+shapeA[shape]+"s that are "+fillA[fill]+" and "+colorA[color]+".");
