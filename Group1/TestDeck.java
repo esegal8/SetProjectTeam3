911f7c75282e12b203a6d107909539c70696addb
rxu8 <rxu8@students.d125.org>
2016-03-14 10:12:45 -0500

    fixes to testDeck class

--- Group1/TestDeck.java
+++ Group1/TestDeck.java
@@ -5,9 +5,13 @@ public class TestDeck
     {
         Deck testDeck= new Deck();
         
-        Card testCard= new Card(); //Card class not written at this point
-        Card testCard1= new Card();
-        Card testCard2= new Card();
+        Card testCard= new Card(1); //Card class not written at this point
+        Card testCard1= new Card(2);
+        Card testCard2= new Card(3);
+        
+        System.out.println("Card ID: "+testCard.getId());
+        System.out.println("Card1 ID: "+testCard1.getId());
+        System.out.println("Card2 ID: "+testCard2.getId());
         
         testDeck.add(testCard);
         testDeck.add(testCard1);
@@ -25,10 +29,10 @@ public class TestDeck
         
         System.out.println(testDeck);
         
-        Iterator<Card> iter= testDeck.iterator();
-        while (iter.hasNext())
+        
+        while (! testDeck.isEmpty())
         {
-            System.out.println(iter.remove);
+           System.out.print(testDeck.takeTop());
         }
         
         System.out.println(testDeck);
