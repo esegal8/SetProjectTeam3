4509e4b0d4be9ef5a21379603014dbfa39770dc1
jmontei8 <jmontei8@students.d125.org>
2016-03-16 10:40:34 -0500

    YAHHHHH

--- Group2/TestZetAnalyzer.java
+++ Group2/TestZetAnalyzer.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * Write a description of class ZetTestAnalyzer here.
  * 
@@ -8,11 +9,23 @@ public class TestZetAnalyzer
 {
     public static void main (String [] args)
     {
-        ZetDeck deck = new ZetDeck();
-        System.out.println(deck+"\n\n");
-        ZetCard c1;
-        ZetCard c2;
-        ZetCard c3;
+        ZetCard c1 = new ZetCard(1,1,1,1);
+        ZetCard c2 = new ZetCard(2,2,2,2);
+        ZetCard c3 = new ZetCard(3,3,3,3);
         
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        
+        System.out.println(ZetAnalyzer.isZet(c1,c2,c3));        
+        
+        c3 = new ZetCard(3,3,3,1);
+        System.out.println(c3);
+        
+        System.out.print(ZetAnalyzer.isZet(c1,c2,c3));
+        
+        //KOOOOOOOOOLLLLLLLLLLLLLLLIIIOOOOOOOOOOOOSIDOURFEARLESSLEADER
+        //GUMMINGER<3GAMMALGARD 
+        //KYUTE
     }
 }
